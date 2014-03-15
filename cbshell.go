b8ea83376e6259d673d419830b3cfad9f1317ec8
Abhishek Singh <singhabhishek.bit@gmail.com>
2014-03-15 16:52:04 +0530

    go fmt + clusterstats api

--- cbshell.go
+++ cbshell.go
@@ -1,88 +1,98 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "io"
-    "os"
-    "path/filepath"
-    "strconv"
-    "strings"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
 
-    "github.com/couchbaselabs/clog"
-    "github.com/couchbaselabs/go-couchbase"
+	"github.com/couchbaselabs/clog"
+	"github.com/couchbaselabs/go-couchbase"
 )
 
 func mf(err error) {
-    if err != nil {
-        clog.Error(err)
-    }
+	if err != nil {
+		clog.Error(err)
+	}
 }
 
 var bucket *couchbase.Bucket
+var pool couchbase.Pool
 
 var url = flag.String("s", "http://localhost:8091", "couchbase server url to connect")
 var bname = flag.String("b", "default", "bucket to connect")
 
 func main() {
-    flag.Parse()
+	flag.Parse()
 
-    cb, err := couchbase.Connect(*url)
-    mf(err)
+	cb, err := couchbase.Connect(*url)
+	mf(err)
 
-    pool, err := cb.GetPool("default")
-    mf(err)
+	pool, err = cb.GetPool("default")
+	mf(err)
 
-    bucket, err = pool.GetBucket(*bname)
-    mf(err)
+	bucket, err = pool.GetBucket(*bname)
+	mf(err)
 
-    HandleInteractiveMode( *url, filepath.Base(os.Args[0]))
+	HandleInteractiveMode(*url, filepath.Base(os.Args[0]))
 }
 
 func execute_internal(url, line string, w io.Writer) error {
 
-    cmdString := strings.Fields(line)
+	cmdString := strings.Fields(line)
 
-    err := performOp(cmdString)
+	err := performOp(url, cmdString)
 
-    if err != nil {
-        clog.Error(err)
-    }
+	if err != nil {
+		clog.Error(err)
+	}
 
-    return err
+	return err
 }
 
-func performOp(cmdString []string) error {
-    cmd := cmdString[0]
+func performOp(url string, cmdString []string) error {
+	cmd := cmdString[0]
 
-    switch cmd {
-    case "Get":
-        key := cmdString[1]
-        var op string
-        err := bucket.Get(key, &op)
-        fmt.Println(op)
-        return err
-    case "Set":
-        key := cmdString[1]
-        TTL, _ := strconv.Atoi(cmdString[2])
-        value := cmdString[3]
-        return bucket.Set(key, TTL, value)
-    case "Delete":
-        key := cmdString[1]
-        return bucket.Delete(key)
-    case "help":
-        Usage()
-    default:
-        fmt.Println("Error: Call not supported")
-        Usage()
-    }
-    return nil
+	switch cmd {
+	case "get":
+		key := cmdString[1]
+		var op string
+		err := bucket.Get(key, &op)
+		fmt.Println(op)
+		return err
+	case "set":
+		key := cmdString[1]
+		TTL, _ := strconv.Atoi(cmdString[2])
+		value := cmdString[3]
+		return bucket.Set(key, TTL, value)
+	case "delete":
+		key := cmdString[1]
+		return bucket.Delete(key)
+	case "nodes":
+        for i := 0; i < len(bucket.NodesJSON); i++ {
+            hostname := bucket.NodesJSON[i].Hostname
+            //stats := bucket.NodesJSON[i].InterestingStats
+            OS := bucket.NodesJSON[i].OS
+            uptime := bucket.NodesJSON[i].Uptime
+            version := bucket.NodesJSON[i].Version
+            fmt.Println("H:", hostname, "OS:", OS, "UP:", uptime, "V:", version)
+        }
+	case "help":
+		Usage()
+	default:
+		fmt.Println("Error: Call not supported")
+		Usage()
+	}
+	return nil
 }
 
 func Usage() {
 
-    fmt.Println("Available commands:\n",
-        "Set <key-name> <TTL> <value>\n",
-        "Get <key-name>\n",
-        "Delete <key-name>\n")
+	fmt.Println("Available commands:\n",
+		"Set <key-name> <TTL> <value>\n",
+		"Get <key-name>\n",
+		"Delete <key-name>\n")
 }
--- interactive.go
+++ interactive.go
@@ -1,56 +1,56 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "os/signal"
-    "syscall"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
-    "github.com/couchbaselabs/clog"
-    "github.com/sbinet/liner"
+	"github.com/couchbaselabs/clog"
+	"github.com/sbinet/liner"
 )
 
 func HandleInteractiveMode(url, prompt string) {
 
-    // Grab HOME environment variable
-    homeDir := os.Getenv("HOME")
-    if homeDir == "" {
-        // then try USERPROFILE for Windows
-        homeDir = os.Getenv("USERPROFILE")
-        if homeDir == "" {
-            fmt.Printf("Unable to determine home directory, history file disabled\n")
-        }
-    }
+	// Grab HOME environment variable
+	homeDir := os.Getenv("HOME")
+	if homeDir == "" {
+		// then try USERPROFILE for Windows
+		homeDir = os.Getenv("USERPROFILE")
+		if homeDir == "" {
+			fmt.Printf("Unable to determine home directory, history file disabled\n")
+		}
+	}
 
-    var liner = liner.NewLiner()
-    defer liner.Close()
+	var liner = liner.NewLiner()
+	defer liner.Close()
 
-    LoadHistory(liner, homeDir)
+	LoadHistory(liner, homeDir)
 
-    go signalCatcher(liner)
+	go signalCatcher(liner)
 
-    for {
-        line, err := liner.Prompt(prompt + "> ")
-        if err != nil {
-            break
-        }
+	for {
+		line, err := liner.Prompt(prompt + "> ")
+		if err != nil {
+			break
+		}
 
-        if line == "" {
-            continue
-        }
+		if line == "" {
+			continue
+		}
 
-        UpdateHistory(liner, homeDir, line)
-        err = execute_internal(url, line, os.Stdout)
-        if err != nil {
-            clog.Error(err)
-        }
-    }
+		UpdateHistory(liner, homeDir, line)
+		err = execute_internal(url, line, os.Stdout)
+		if err != nil {
+			clog.Error(err)
+		}
+	}
 }
 
 func signalCatcher(liner *liner.State) {
-    ch := make(chan os.Signal)
-    signal.Notify(ch, syscall.SIGINT)
-    <-ch
-    liner.Close()
-    os.Exit(0)
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGINT)
+	<-ch
+	liner.Close()
+	os.Exit(0)
 }
